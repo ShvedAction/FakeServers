170feff375d9b5b63b07a1736bd5ecda13378ae6
Шведюк Алексей Александрович <aashvedy@mts.ru>
2017-02-22 10:02:05 +0300

    Tests for GetReciveHistory

--- FakeHttpServerTests/TestsForAnyIFakeServer.cs
+++ FakeHttpServerTests/TestsForAnyIFakeServer.cs
@@ -1,12 +1,16 @@
 ﻿using FakeServers;
 using FakeServers.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Net;
 
 namespace FakeHttpServerTests
 {
     public abstract class TestsForAnyIFakeServer
     {
+
+        const string RESPONSE_SEMPLE_BODY = "value of fake server response";
+        const string REQUEST_SAMPLE_BODY = "value of request";
         protected virtual string ListenedFakeServerURL { get;}
 
         //[ExpectedException(typeof(AssertFailedException))]
@@ -23,8 +27,6 @@ namespace FakeHttpServerTests
 
         protected void any_IFakeServer_should_return_set_response()
         {
-            const string RESPONSE_SEMPLE_BODY = "value of fake server response";
-            const string REQUEST_SAMPLE_BODY = "value of request";
             using (IFakeServer fakeserver = BuildTestingServer())
             {
                 fakeserver.ShouldRecived().Post(REQUEST_SAMPLE_BODY).Response(RESPONSE_SEMPLE_BODY);
@@ -35,8 +37,6 @@ namespace FakeHttpServerTests
 
         protected void any_IFakeServer_should_return_set_response_headers()
         {
-            const string RESPONSE_SEMPLE_BODY = "value of fake server response";
-            const string REQUEST_SAMPLE_BODY = "value of request";
             const string EXAMPLE_HEADER_KEY = "Content-Type";
             const string EXAMPLE_HEADER_VALUE = "text/xml";
             string[] exampleResponseHeaders = new string[] { EXAMPLE_HEADER_KEY+": "+ EXAMPLE_HEADER_VALUE };
@@ -51,8 +51,6 @@ namespace FakeHttpServerTests
 
         protected void any_IFakeServer_should_stop_without_exception()
         {
-            const string RESPONSE_SEMPLE_BODY = "value of fake server response";
-            const string REQUEST_SAMPLE_BODY = "value of request";
             IFakeServer fakeserver = BuildTestingServer();
             fakeserver.ShouldRecived().Post(REQUEST_SAMPLE_BODY).Response(RESPONSE_SEMPLE_BODY);
             var response = HttpSender.SendPost(ListenedFakeServerURL, REQUEST_SAMPLE_BODY);
@@ -60,6 +58,40 @@ namespace FakeHttpServerTests
 
             fakeserver.CheckAllReciverConditional();
             fakeserver.stopServer();
+        }
+
+        protected void any_IFakeServer_should_able_to_get_history_of_ricivers_messages()
+        {
+            IFakeServer fakeserver = BuildTestingServer();
+            fakeserver.ShouldRecived();
+            HttpSender.SendPost(ListenedFakeServerURL, REQUEST_SAMPLE_BODY);
+            string[] actualReciveMessages = fakeserver.GetReciveHistory();
+            Assert.AreEqual(1, actualReciveMessages.Length, "Wrong count of element for array was returned by method GetReciveHistory");
+            Assert.AreEqual(REQUEST_SAMPLE_BODY, actualReciveMessages[0], "Wrong content of element for array was returned by method GetReciveHistory");
+            try
+            {
+                fakeserver.stopServer();
+            }
+            catch (Exception)
+            {
+                //Сервер должен сказать что не все запросы пришли. Но в данном тесте это не проверяется.
+            }
+        }
+
+        protected void any_IFakeServer_should_return_deffault_message_for_no_mathced_requests()
+        {
+            IFakeServer fakeserver = BuildTestingServer();
+            fakeserver.ShouldRecived();
+            string actualDeffaultMessage = HttpSender.SendPost(ListenedFakeServerURL, REQUEST_SAMPLE_BODY);
+            Assert.AreEqual(1, actualDeffaultMessage, "Wrong DeffaultMessage");
+            try
+            {
+                fakeserver.stopServer();
+            }
+            catch (Exception)
+            {
+                //Сервер должен сказать что не все запросы пришли. Но в данном тесте это не проверяется.
+            }
         }
     }
 }
